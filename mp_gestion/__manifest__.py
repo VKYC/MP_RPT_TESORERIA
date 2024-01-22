d5ae787b48acdb532eae1836b8b836df2b5844f6
Tonny <corner.store59@gmail.com>
2024-01-22 15:39:11 -0600

    [ADD] Se agrega el modulo de gestion MP

---
+++ mp_gestion/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "MP Gestion",
+
+    'summary': """Se agrega la gestion para la contabilidad""",
+    'author': "Tonny Velazquez",
+    'website': "corner.store59@gmail.com",
+    'category': 'accounting',
+    'version': '15.0.0.0.1',
+    'depends': ['account'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/account_move_views.xml',
+        # 'views/templates.xml',
+    ],
+    'license': 'Other proprietary',
+}
