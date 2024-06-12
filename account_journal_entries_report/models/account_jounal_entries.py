9855b6828130673298c307323a0668ca3a48e221
BarucAlvarezDev <nodraruc@outlook.com>
2024-06-11 19:30:43 -0600

    [MOD] Se modifica campo de filtro de wizard

--- account_journal_entries_report/models/account_jounal_entries.py
+++ account_journal_entries_report/models/account_jounal_entries.py
@@ -7,8 +7,8 @@ class AccountMove(models.Model):
     @api.model
     def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
         if 'initial_date' in self._context and 'final_date' in self._context:
-            domain += [('date', '>=', self._context['initial_date']),
-                       ('date', '<=', self._context['final_date'])]
+            domain += [('create_date', '>=', self._context['initial_date']),
+                       ('create_date', '<=', self._context['final_date'])]
         return super(AccountMove, self).read_group(domain, fields, groupby,
                                                                  offset=offset, limit=limit,
                                                                  orderby=orderby, lazy=lazy)
