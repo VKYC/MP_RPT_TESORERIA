5413125729efbbfb65aac26254fa48f24a0fd8b2
BarucAlvarezDev <nodraruc@outlook.com>
2024-06-15 13:12:54 -0600

    [ADD] Reporte PDF del Libro de Diario v1.0

--- account_journal_entries_report/__manifest__.py
+++ account_journal_entries_report/__manifest__.py
@@ -7,6 +7,7 @@
     'depends': ['account'],
     "data": [
         "security/ir.model.access.csv",
+        "report/account_journal_entries_report.xml",
         "wizard/account_journal_entries_date_wizard.xml",
         "views/account_journal_entries_report.xml",
         "views/account_journal_entries_menuitem.xml",
--- account_journal_entries_report/models/account_jounal_entries.py
+++ account_journal_entries_report/models/account_jounal_entries.py
@@ -1,14 +1,19 @@
-from odoo import models, api
+from odoo import models, fields, api
+from odoo.tools import date_utils
 
 
 class AccountMove(models.Model):
     _inherit = 'account.move.line'
+
+    partner_name = fields.Char(related='move_id.partner_id.display_name')
+    move_line_ids = fields.One2many(related='move_id.line_ids')
 
     @api.model
     def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
         if 'initial_date' in self._context and 'final_date' in self._context:
             domain += [('create_date', '>=', self._context['initial_date']),
                        ('create_date', '<=', self._context['final_date'])]
+
         return super(AccountMove, self).read_group(domain, fields, groupby,
                                                                  offset=offset, limit=limit,
                                                                  orderby=orderby, lazy=lazy)
