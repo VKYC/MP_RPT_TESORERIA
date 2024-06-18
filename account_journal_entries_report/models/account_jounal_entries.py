7acd6c671d1ff1e23af71ad8c8d67b2fee7032e2
BarucAlvarezDev <nodraruc@outlook.com>
2024-06-17 19:21:22 -0600

    [CHG] Filtro de fecha cambiado

--- account_journal_entries_report/models/account_jounal_entries.py
+++ account_journal_entries_report/models/account_jounal_entries.py
@@ -11,8 +11,8 @@ class AccountMove(models.Model):
     @api.model
     def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
         if 'initial_date' in self._context and 'final_date' in self._context:
-            domain += [('create_date', '>=', self._context['initial_date']),
-                       ('create_date', '<=', self._context['final_date'])]
+            domain += [('date', '>=', self._context['initial_date']),
+                       ('date', '<=', self._context['final_date'])]
 
         return super(AccountMove, self).read_group(domain, fields, groupby,
                                                                  offset=offset, limit=limit,
